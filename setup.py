aa50b892d2ef47fadf2ccb1b2fadfc7eec7b79c4
Vladimir Mihailenco <vladimir.webdev@gmail.com>
2012-04-06 17:12:46 +0300

    Update project description

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ readme = open('README.rst').read()
 setup(
     name='ndbunq',
     version='0.1.0',
-    description='Poor man unique constraint for NDB',
+    description='Poor man unique constraints for Google Appengine NDB',
     long_description=readme,
     author='Vladimir Mihailenco',
     author_email='vladimir.webdev@gmail.com',
